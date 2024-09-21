886b4e33196b639b19716433d08a0f17ae647a89
Claudio Leite <claudio.leite@hotmail.com>
2024-09-21 13:30:21 +0200

    test

--- src/components/Form.js
+++ src/components/Form.js
@@ -17,6 +17,8 @@ export function FeedbackForm() {
       <input type="hidden" name="form-name" value="feedback" />
       <input name="name" type="text" placeholder="Name" required />
       <input name="email" type="text" placeholder="Email (optional)" />
+      <input name="message" type="text" placeholder="text" required />
+
       <button type="submit">Submit</button>
     </form>
   );
