93db2eb142b3799c8a23bb0ff6f521885169283e
Claudio Leite <claudio.leite@hotmail.com>
2024-09-22 13:28:44 +0200

    netlify atribut added to form

--- src/components/Form.js
+++ src/components/Form.js
@@ -13,7 +13,7 @@ function FeedbackForm() {
   };
 
   return (
-    <form name="feedback" onSubmit={handleFormSubmit}>
+    <form name="feedback" netlify onSubmit={handleFormSubmit}>
       <input type="hidden" name="form-name" value="feedback" />
       <input name="name" type="text" placeholder="Name" required />
       <input name="email" type="text" placeholder="Email (optional)" />
